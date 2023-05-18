43a3fe3dffc1c1e511396eb9fb630721c94e9a5c
bsquillari <bsquillari@itba.edu.ar>
2023-05-18 18:41:09 -0300

    add: Logs Bucket + site buckets arguments Co-authored-by: gbudoberra <gbudoberra@itba.edu.ar> Co-authored-by: fzimbimbakis <fzimbimbakis@itba.edu.ar> Co-authored-by: shadad <shadad@itba.edu.ar>

--- organization/locals.tf
+++ organization/locals.tf
@@ -7,35 +7,53 @@ locals {
   path = "../resources"
   lambdas = {
     "get_pets" = {
-      filename = "${local.path}/lambda/lambda_get_pets.zip"
+      filename      = "${local.path}/lambda/lambda_get_pets.zip"
       function_name = "get_pets"
-      method = "GET"
-      handler = "lambda_get_pets.main"
-      path = "/pets"
-      part_path = "pets" 
+      method        = "GET"
+      handler       = "lambda_get_pets.main"
+      path          = "/pets"
+      part_path     = "pets"
       # TODO: no se que son estos ultimos 3
     },
     "post_pet" = {
-      filename = "${local.path}/lambda/lambda_post_pet.zip"
+      filename      = "${local.path}/lambda/lambda_post_pet.zip"
       function_name = "post_pet"
-      method = "POST"
-      handler = "lambda_post_pet.main"
-      path = "/pets"
-      part_path = "pets" 
+      method        = "POST"
+      handler       = "lambda_post_pet.main"
+      path          = "/pets"
+      part_path     = "pets"
       # TODO: no se que son estos ultimos 3
     }
   }
 
   # API GW
   apigw = {
-    name = "main_api_gw",
+    name        = "main_api_gw",
     description = "Main API gateway",
   }
 
   # Site bucket
-  site_bucket = {
-    prefix = "adoptemos-todos-s3-",
+  buckets = {
+    site_bucket = {
+      prefix = "adoptemos-todos-site-s3-",
+    }
+    logs_bucket = {
+      prefix = "adoptemos-todos-logs-s3-",
+      acl    = "log-delivery-write"
+    }
+    default_server_side_encryption = {
+      rule = {
+        apply_server_side_encryption_by_default = {
+          sse_algorithm = "AES256"
+        }
+      }
+    }
+    logs_prefix = "log/"
   }
+
+
+
+
   filetypes = {
     "html" : "text/html",
     "jpg" : "image/jpg",
@@ -49,7 +67,7 @@ locals {
   file_with_type = flatten([
     for type, mime in local.filetypes : [
       for key, value in fileset("../resources/web/", "**/*.${type}") : {
-        mime = mime
+        mime      = mime
         file_name = value
       }
     ]
