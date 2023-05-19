8931e485ee1fbff5fac2c4ce5bf4a5177f1e315f
bsquillari <bsquillari@itba.edu.ar>
2023-05-19 13:38:46 -0300

    hotfix: Added internet gw

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,9 +5,16 @@ resource "aws_vpc" "this" {
   }
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    Name = "internet_gw"
+  }
+}
+
 resource "aws_default_network_acl" "this" {
   default_network_acl_id = aws_vpc.this.default_network_acl_id
-  
+
   ingress{
     protocol = -1
     rule_no = 100
@@ -32,6 +39,7 @@ resource "aws_subnet" "this" {
 
   vpc_id            = aws_vpc.this.id
   availability_zone = local.availability_zones[count.index]
+  # TODO public?
   cidr_block        = local.public_subnets[count.index]
 
   tags = {
